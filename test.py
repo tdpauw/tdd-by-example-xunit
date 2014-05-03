34bd245d1ee13ee1ca60358ac322fd6c3c97f004
Thierry de Pauw <thierry.depauw@wayofthinking.net>
2014-05-03 16:33:41 +0200

    introduce log string to check that the testMethod was run

--- test.py
+++ test.py
@@ -21,6 +21,7 @@ class WasRun(TestCase):
 
 	def testMethod(self):
 		self.wasRun = 1
+		self.log = self.log + "testMethod "
 
 class TestCaseTest(TestCase):
 	def setUp(self):
@@ -32,7 +33,7 @@ class TestCaseTest(TestCase):
 
 	def testSetUp(self):
 		self.test.run()
-		assert("setUp " == self.test.log)
+		assert("setUp testMethod " == self.test.log)
 
 TestCaseTest("testRunning").run()
 TestCaseTest("testSetUp").run()
