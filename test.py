4f8188d5ba8853c3acebf6d50b83a49e93bf3282
Thierry de Pauw <thierry.depauw@wayofthinking.net>
2014-05-03 16:02:01 +0200

    introduce TestCaseTest class in order to test WasRun

--- test.py
+++ test.py
@@ -15,8 +15,11 @@ class WasRun(TestCase):
 	def testMethod(self):
 		self.wasRun = 1
 
+class TestCaseTest(TestCase):
+	def testRunning(self):
+		test = WasRun("testMethod")
+		assert(not test.wasRun)
+		test.run()
+		assert(test.wasRun)
 
-test = WasRun("testMethod")
-assert(not test.wasRun)
-test.run()
-assert(test.wasRun)
+TestCaseTest("testRunning").run()
