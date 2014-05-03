7bbb554073d77e353f7d4b32cbff0e680d134109
Thierry de Pauw <thierry.depauw@wayofthinking.net>
2014-05-03 21:38:37 +0200

    introduce child test to report failure

--- test.py
+++ test.py
@@ -21,12 +21,16 @@ class TestCase:
 class TestResult:
 	def __init__(self):
 		self.runCount = 0
+		self.errorCount = 0
 
 	def testStarted(self):
 		self.runCount = self.runCount + 1
 
+	def testFailed(self):
+		self.errorCount = self.errorCount + 1
+
 	def summary(self):
-		return "%d run, 0 failed" % self.runCount
+		return "%d run, %d failed" % (self.runCount, self.errorCount)
 
 class WasRun(TestCase):
 	def __init__(self, name):
@@ -60,6 +64,13 @@ class TestCaseTest(TestCase):
 		result = test.run()
 		assert("1 run, 1 failed" == result.summary())		
 
+	def testFailedResultFormatting(self):
+		result = TestResult()
+		result.testStarted()
+		result.testFailed()
+		assert("1 run, 1 failed" == result.summary())		
+
 TestCaseTest("testTemplateMethod").run()
 TestCaseTest("testResult").run()
-TestCaseTest("testFailedResult").run()
+#TestCaseTest("testFailedResult").run()
+TestCaseTest("testFailedResultFormatting").run()
