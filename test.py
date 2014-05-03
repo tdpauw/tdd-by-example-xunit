2bbc59817ec2b707d821e0b35a36ec52204215ba
Thierry de Pauw <thierry.depauw@wayofthinking.net>
2014-05-03 21:24:18 +0200

    increment TestResult.runCount each time a test is run

--- test.py
+++ test.py
@@ -7,18 +7,23 @@ class TestCase:
 		pass
 
 	def run(self):
+		result = TestResult()
+		result.testStarted()
 		self.setUp()
 		method = getattr(self, self.name)
 		method()
 		self.tearDown()
-		return TestResult()
+		return result
 
 	def tearDown(self):
 		pass
 
 class TestResult:
 	def __init__(self):
-		self.runCount = 1
+		self.runCount = 0
+
+	def testStarted(self):
+		self.runCount = self.runCount + 1
 
 	def summary(self):
 		return "%d run, 0 failed" % self.runCount
