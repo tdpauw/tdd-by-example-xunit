3602d42c21f52fcc567e0153c7a5366720feb20f
Thierry de Pauw <thierry.depauw@wayofthinking.net>
2014-05-05 21:41:51 +0200

    print summary of each test run

--- test.py
+++ test.py
@@ -75,7 +75,7 @@ class TestCaseTest(TestCase):
 		result.testFailed()
 		assert("1 run, 1 failed" == result.summary())		
 
-TestCaseTest("testTemplateMethod").run()
-TestCaseTest("testResult").run()
-TestCaseTest("testFailedResult").run()
-TestCaseTest("testFailedResultFormatting").run()
+print TestCaseTest("testTemplateMethod").run().summary()
+print TestCaseTest("testResult").run().summary()
+print TestCaseTest("testFailedResult").run().summary()
+print TestCaseTest("testFailedResultFormatting").run().summary()
